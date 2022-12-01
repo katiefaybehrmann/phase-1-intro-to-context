8dcfea702cde8aa6212728eba35e1032c52fbd80
Katie <katiefaybehrmann@gmail.com>
2022-12-01 11:29:28 -0700

    commit tree 1ddff6cb9a03fb409c09950ffef1fdcfa1803ebc

--- index.js
+++ index.js
@@ -1,1 +1,96 @@
 // Your code here
+let employeeObj;
+
+
+function createEmployeeRecord(array) {
+    employeeObj = {
+        "firstName": array[0],
+        "familyName": array[1],
+        "title": array[2],
+        "payPerHour": array[3],
+        "timeInEvents": [],
+        "timeOutEvents": []
+    }
+    return employeeObj;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    let newArray = [];
+    arrayOfArrays.forEach((array) => {
+        newArray.push(createEmployeeRecord(array))
+    })
+    return newArray;
+
+}
+
+function createTimeInEvent(employeeObj, date) {
+    let dateArray = date.split(' ')
+    let hour = parseInt(dateArray[1], 10)
+    let day = dateArray[0]
+    let timeInObj = {
+        "type": "TimeIn",
+        "hour": hour,
+        "date": day,
+    }
+    employeeObj.timeInEvents.push(timeInObj)
+    return employeeObj;
+}
+
+function createTimeOutEvent(employeeObj, date) {
+    let dateArray = date.split(' ')
+    let hour = parseInt(dateArray[1], 10)
+    let day = dateArray[0]
+    let timeOutObj = {
+        "type": "TimeOut",
+        "hour": hour,
+        "date": day
+    }
+    employeeObj.timeOutEvents.push(timeOutObj)
+    return employeeObj;
+
+}
+
+function hoursWorkedOnDate(employeeObj, date) {
+    let num;
+    for (let i = 0; i < employeeObj.timeInEvents.length; i++) {
+        if (employeeObj.timeInEvents[i].date === date) {
+            num = employeeObj.timeOutEvents[i].hour - employeeObj.timeInEvents[i].hour
+            return num / 100;
+        }
+    }
+    return false;
+}
+
+function wagesEarnedOnDate(employeeObj, date) {
+    let num = hoursWorkedOnDate(employeeObj, date)
+    num = num * employeeObj.payPerHour
+    return num;
+
+}
+
+function allWagesFor(employeeObj) {
+    let wages = [];
+    for (let i = 0; i < employeeObj.timeInEvents.length; i++) {
+        wages.push(wagesEarnedOnDate(employeeObj, employeeObj.timeInEvents[i].date))
+    }
+    let num = wages.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
+
+    return num;
+
+}
+
+function calculatePayroll(employeeArray) {
+    let num;
+    let allWages = [];
+    employeeArray.forEach(x => allWages.push(allWagesFor(x)))
+
+    num = allWages.reduce((accumulator, currentValue) => accumulator + currentValue,
+    0
+  )
+  return num
+
+
+}
